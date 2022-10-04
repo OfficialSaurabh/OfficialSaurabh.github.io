67ce881d37d2363815b6314e7a3087f74e99b553
Saurabh Kumar~ <hmsaurabhkr@gmail.com>
2022-10-04 23:47:55 +0530

    add tech stack tags

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const data = [
         description: "It doesn't get more simple than this. Save any and all thoughts, ideas, recipes in one app: NoteSnap",
         sourceCode: "https://github.com/OfficialSaurabh/Notesnap",
         livePreview: "https://notesnap.netlify.app/",
-        techStack: ["HTML" , " CSS" , " JavaScript"]
+        techStack: ["HTML" , "CSS", "JavaScript"]
     },
     {
         "id": 2,
@@ -15,17 +15,24 @@ const data = [
         description: "Single Page Real Estate “Rent a Property” web app with a filtering mechanism.",
         sourceCode: "https://github.com/OfficialSaurabh/Rent-a-Property",
         livePreview: "https://view-property.netlify.app/",
-        techStack:["React JS", " Tailwind CSS", " Context API"]
-    }
-    ,
+        techStack:["React JS", "Tailwind CSS", "Context API"]
+    },
     {
         "id": 3,
         title: "Fake Shop",
         image:"./img/project/project3.png",
-
         description: "Fake Shop is 2-page web app one to display a list of products and second to show details of a given product",
         sourceCode: "https://github.com/OfficialSaurabh/fakeshop",
         livePreview: "https://see-product.netlify.app/",
-        techStack:["React JS", " Tailwind CSS", " Fake Store API"]
+        techStack:["React JS", "Tailwind CSS", " Fake Store API"]
+    },
+    {
+        "id": 3,
+        title: "Fake Shop",
+        image:"./img/project/project3.png",
+        description: "Fake Shop is 2-page web app one to display a list of products and second to show details of a given product",
+        sourceCode: "https://github.com/OfficialSaurabh/fakeshop",
+        livePreview: "https://see-product.netlify.app/",
+        techStack:["React JS", "Tailwind", " Fake Store API"]
     }
 ];
--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const container = document.getElementById("cards");
 
-data.forEach((result, idx) => {
+data.forEach((result) => {
   // Create card element
   const card = document.createElement("div");
   card.classList = "card-body";
+  // set every tech stack a specific color
+  
+  const techstack = document.querySelectorAll(".techstack");
+  const colors = ["#E34C26", "#2965f1", "#f0db4f",  "#61dafb", "#4dc0b5", "#4dc0b5"];
+  techstack.forEach((item, id) => {
+      if (item.innerText === "HTML") {
+          item.style.color = colors[0];
+        } else if (item.innerText === "CSS") {
+              item.style.color = colors[1];
+            } else if (item.innerText === "JavaScript") {
+                  item.style.color = colors[2];
+                } else if (item.innerText === "React JS") {
+                    //   item.style.backgroundColor = colors[3];
+                      item.style.color = colors[3];
+                    } else if (item.innerText === "Next JS") {
+                          item.style.color = colors[4];
+                        } else if (item.innerText === "Tailwind CSS") {
+                              item.style.color = colors[5];
+                            } else if (item.innerText === "MongoDB") {
+                                    item.style.color = colors[6];
+                                    }
+                                    console.log(item);
+  });
 
   // Construct card content
   const content = `
@@ -15,7 +38,10 @@ data.forEach((result, idx) => {
                 ${result.description}
               </p>
               <h3 class="stack">Tech Stack</h3>
-                <span class="techstack"> ${result.techStack} </span> 
+
+                <div class="stack-list">
+                    ${result.techStack.map((item) => `<span class="techstack">${item}</span>`).join("")}
+                </div>
               <hr>
               <div class="card-foot">
                 <a href="${result.sourceCode}" class="sourcecode-btn" target="_blank"><i class="fa-brands fa-github"></i> Source Code</a >
@@ -25,7 +51,6 @@ data.forEach((result, idx) => {
               </div>
           </div>
     `;
-
   // Append newyly created card element to the container
   container.innerHTML += content;
 });
